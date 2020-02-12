9a9e5a89f2a2af7d6494395eac7d0b98075b22a9
ivanhet <60697415+ivanhet@users.noreply.github.com>
2020-02-12 12:14:43 -0300

    Add files via upload

---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,22 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAnio = document.getElementById('mes').value;
+
+switch (mesDelAnio)
+
+{
+        case "Febrero":
+        alert("Este mes no tiene mas de 29 dias")
+        break;
+
+        default: 
+        alert("Este mes tiene 30 dias o mas")
+        
+
+
+	
+	
+
+}
+}//FIN DE LA FUNCIÓN
